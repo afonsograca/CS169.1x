c6c5cd265a378c6b84d3ad7e99c3fdd77fc1b7a3
Afonso Graça <afonso.graca@me.com>
2014-06-08 15:09:50 +0100

    hw4 first scenario done

---
+++ HW4/bdd-tdd-cycle/rottenpotatoes/features/step_definitions/movie_steps.rb
@@ -0,0 +1,13 @@
+# Add a declarative step here for populating the DB with movies.
+
+Given /the following movies exist/ do |movies_table|
+  movies_table.hashes.each do |movie|
+    # each returned element will be a hash whose key is the table header.
+    # you should arrange to add that movie to the database here.
+    Movie.create(movie)
+  end
+end
+
+Then(/^the director of "(.*?)" should be "(.*?)"$/) do |arg1, arg2|
+  Movie.find_by_title(arg1).director.should have_content(arg2)
+end
